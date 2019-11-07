2b54a4578af499a368a4010dbe1b550b6ef97eb5
Fr4k26 <afrak@mail.bradley.edu>
2019-11-07 15:21:02 -0600

    Movement Script for player.

---
+++ Untouchable/Untouchable/Assets/Scripts/Movement.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Variables for how the character moves.
+    public float speed;
+    private float horizInput;
+    private float vertInput;
+    private Vector3 ForwardMovement;
+    private Vector3 RightMovement;
+    private Rigidbody rb;
+    private Vector3 velocity;
+
+    // Forces that involves the player.
+    public float jumpForce;
+    public float gravForce;
+    public bool grounded;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Sets the player as grounded so they are allowed to jump.
+        grounded = true;
+
+        // Players rigidbody. Yep.
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Updates when the Player is on the horizontal/vertical axis.
+        horizInput = Input.GetAxis("Horizontal");
+        vertInput = Input.GetAxis("Vertical");
+
+        // Function that allows player to jump.
+        Jump();
+
+        // Function that adds "weight" to the player when they are coming back down.
+        Gravity();
+    }
+
+    private void FixedUpdate()
+    {
+        // Allows the player to move forward and backwards.
+        ForwardMovement = transform.forward * vertInput;
+
+        // Allows the player to move left and right.
+        RightMovement = transform.right * horizInput;
+
+        // Allows the player to move around, and the speed at which they are allowd to move.
+        velocity = (RightMovement + ForwardMovement).normalized * speed;
+
+        // Moves the rigidbody towards a position.
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
+
+    void Jump()
+    {
+        // Adds a force when the player presses space, and they are on the platform.
+        if (Input.GetKeyDown("space") && grounded == true)
+        {
+            rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+            grounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // If player collides with the platform, they are allowed to jump again.
+        if(collision.gameObject.CompareTag("Platform"))
+        {
+            grounded = true;
+        }
+    }
+
+    void Gravity()
+    {
+        // When the player is in the air, it adds a downward force to add "weight" to the player.
+        if(grounded == false)
+        {
+            rb.AddForce(0, -gravForce, 0, ForceMode.Impulse);
+        }
+    }
+        
+
+}

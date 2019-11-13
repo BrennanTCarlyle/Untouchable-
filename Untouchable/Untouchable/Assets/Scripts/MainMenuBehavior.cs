54ec47c8f10c4a5450b337ab4093f8fa8def83fb
BrennanTCarlyle <brennan.carlyle@gmail.com>
2019-11-13 00:57:11 -0900

    Added additional folders, UI & Art Assets, as well as new main menu scenes

---
+++ Untouchable/Untouchable/Assets/Scripts/MainMenuBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBehavior : MonoBehaviour
+{
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    /// <summary>
+    /// Uses pound define to quit the game in either the editor, or the application,
+    /// depending on which is playing the game 
+    /// </summary>
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}

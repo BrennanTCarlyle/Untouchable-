473c23fe6cffa76971f0212a6f4e49382bcca335
Jake Velicer <007velicer@gmail.com>
2019-11-19 01:13:58 -0600

    Show Cursor

---
+++ Untouchable/Untouchable/Assets/Scripts/ShowCursor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = Cursor.visible;
+    }
+
+}

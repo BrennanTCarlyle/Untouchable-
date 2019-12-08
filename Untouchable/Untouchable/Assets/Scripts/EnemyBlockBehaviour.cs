6554bc84ec1a9f7caceab6eae2f33671dde0825b
Mike Romeo <mikeromeo@socialbrothers.nl>
2019-12-08 15:48:50 -0600

    movingblock adjustments

--- Untouchable/Untouchable/Assets/Scripts/EnemyBlockBehaviour.cs
+++ Untouchable/Untouchable/Assets/Scripts/EnemyBlockBehaviour.cs
@@ -9,12 +9,17 @@ public class EnemyBlockBehaviour : MonoBehaviour
 
     public int killTimer = 3;
 
+    public float startDelay = 1;
+    private bool canStart = false;
+
     [Header("Refs to all moving blocks")]
     public List<GameObject> movingBlocks;
 
     // Start is called before the first frame update
     void Start()
     {
+        canStart = false;
+        Invoke("startMoving", startDelay);
         Invoke("killSelf", killTimer);
 
         // turn off one of three blocks
@@ -24,7 +29,15 @@ public class EnemyBlockBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.back * Time.deltaTime * speed);
+        if (canStart)
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+    }
+
+    void startMoving()
+    {
+        canStart = true;
     }
 
     void killSelf()

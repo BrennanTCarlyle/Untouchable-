65042f34a4fb0c54e34e9e4d778a17bee00f4e23
Jake Velicer <007velicer@gmail.com>
2019-12-08 19:00:19 -0600

    Pause menu cursor show functionality

--- Untouchable/Untouchable/Assets/Scripts/PauseMenuBehavior.cs
+++ Untouchable/Untouchable/Assets/Scripts/PauseMenuBehavior.cs
@@ -13,7 +13,7 @@ public class PauseMenuBehavior : MainMenuBehavior
         // The game is not paused 
         isPaused = false;
 
-        // The Pause Menu object is inactive 
+        // The Pause Menu object is inactive
         pauseMenu.SetActive(false);
 
         // Timescale is set to 1, meaning objects can move in the game 
@@ -34,10 +34,14 @@ public class PauseMenuBehavior : MainMenuBehavior
         if (isPaused == true)
         {
             Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
         }
         else
         {
             isPaused = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
             Time.timeScale = 1;
             pauseMenu.SetActive(false);
 
